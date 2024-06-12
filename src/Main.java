c274b52409651a36dbdb9ec2be008ac9eb0b12cb
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2024-06-11 20:55:37 -0400

    java loops Lab completed

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,16 @@ public class Main {
     public static void main(String[] args) {
 
         int[] numbersArray = {2, 5, 3, -7, 9, 5, -2, 5, 6};
-        int differenceNumberResult =  largestAndSmallerDifference(numbersArray);
+        int differenceNumberResult =  largestAndSmallestNumberDifference(numbersArray);
         System.out.println("the difference between largest and smallest numbers is: " + differenceNumberResult);
+
+        smallestAndSecondSmallestNumber(numbersArray);
+        double result = mathProblemSolver(2,  2);
+        System.out.println("The result of the math is: " + result);
 
     }
 
-    public static int largestAndSmallerDifference(int[] numbersArray) {
+    public static int largestAndSmallestNumberDifference(int[] numbersArray) {
 
         int smallest = Integer.MAX_VALUE;
         int largest = Integer.MIN_VALUE;
@@ -24,4 +28,30 @@ public class Main {
 //        System.out.println("Largest: " + largest);
         return  largest - smallest;
     }
+
+    public static void smallestAndSecondSmallestNumber (int[] numbersArray) {
+
+        int smallestNum = Integer.MAX_VALUE;
+        int secondSmallestNum = Integer.MAX_VALUE;
+
+        for(int i = 0; i < numbersArray.length; i++){
+            if(numbersArray[i] < smallestNum) {
+                secondSmallestNum = smallestNum;
+                smallestNum = numbersArray[i];
+            } else if(numbersArray[i] < secondSmallestNum && numbersArray[i] != smallestNum){
+                secondSmallestNum = numbersArray[i];
+            }
+        }
+        System.out.println("Smaller number: " + smallestNum);
+        System.out.println("Second Smallest number: " + secondSmallestNum);
+    };
+    public static double mathProblemSolver(int x, int y){
+        int expo = (x*x);
+        double div = (4.0 * y) / 5;
+        double minus = div - x;
+        double expo2 = minus * minus;
+
+        return expo + expo2;
+
+    };
 }
